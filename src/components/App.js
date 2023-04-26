1950b505cfb523d67d2bc51af79262a4d94d447b
sunny8750 <sunnyrao8750@gmail.com>
2023-04-26 15:42:28 +0530

    submitting code-Wed Apr 26 2023 15:42:27 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,84 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState, useEffect } from 'react';
+import './styles.css';
 
+const reviewsData = [
+  {
+    id: 1,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+    name: 'maria ferguson',
+    title: 'office manager',
+    quote:
+      'Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.',
+  },
+  {
+    id: 2,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    name: 'john doe',
+    title: 'regular guy',
+    quote:
+      'Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.',
+  },
+  {
+    id: 3,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959121/person-1_aufeoq.jpg',
+    name: 'peter smith',
+    title: 'product designer',
+    quote:
+      'Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.',
+  },
+  {
+    id: 4,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    name: 'susan andersen',
+    title: 'the boss',
+    quote:
+      'Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ',
+  },
+];
+
+const ReviewsSlider = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setActiveIndex((prevIndex) => (prevIndex + 1) % reviewsData.length);
+    }, 3000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const handlePrev = () => {
+    setActiveIndex((prevIndex) =>
+      prevIndex === 0 ? reviewsData.length - 1 : prevIndex - 1
+    );
+  };
+
+  const handleNext = () => {
+    setActiveIndex((prevIndex) =>
+      prevIndex === reviewsData.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+}
+  return (
+    <div>
+      <h2 id="review-heading">Reviews</h2>
+      <div id="review-container">
+        {reviewsData.map((review, index) => (
+          <div
+            key={review.id}
+            className={`review ${index === activeIndex ? 'active' : ''}`}
+          >
+            <img
+              id={`person-${index}-image`}
+              src={review.image}
+              alt={review.name}
+            />
+            <h3 id={`person-${index}-name`}>{review.name}</h3>
+            <h4>{review.title}</h4>
+            <p>{review.quote}</p>
+          </div>
+        ))})
